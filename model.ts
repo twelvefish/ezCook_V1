86d4fbb9d613920e1b22e9a6af5bda21d2e4269e
twelvefish <amingo08131@gmail.com>
2018-11-15 07:35:49 +0800

    move files

---
+++ model.ts
@@ -0,0 +1,22 @@
+export type Member = {
+    id: string
+    name: string
+    phone: string
+    email: string
+    lineId: string
+    joinTime: string
+    checkEmail: string
+}
+
+export type Chef = {
+    id: string
+    name: string
+    phone: string
+    email: string
+    lineId: string
+    foodKind: string
+    introduction: string
+}
+
+
+
---
+++ sheetColumn.ts
@@ -0,0 +1,53 @@
+export const memberColumn = {
+    workspace: "會員",
+    sheetId: "1lp2-Qmr3DZWY7ynAMMW4m5ygNo0koc2yVa66PMmnRqI",
+    gid: "0",
+    id: "A",
+    name: "B",
+    phone: "C",
+    email: "D",
+    lineId: "E",
+    joinTime: "F",
+    checkEmail: "G"
+}
+
+export const chefColumn = {
+    workspace: "廚師",
+    sheetId: "1lp2-Qmr3DZWY7ynAMMW4m5ygNo0koc2yVa66PMmnRqI",
+    gid: "1136164279",
+    id: "A",
+    name: "B",
+    phone: "C",
+    email: "D",
+    lineId: "E",
+    foodKind: "F",
+    introduction: "G"
+}
+
+export const groupColumn = {
+    workspace: "群組",
+    sheetId: "1iziPQCCuphRpCze78cyNpk--V5h915-ny89OcFdYCVo",
+    gid: "786775329",
+    id: "A",
+    groupName: "B",
+    groupLineId: "C",
+    adminName: "D",
+}
+
+export const visitorColumn = {
+    workspace: "來訪紀錄",
+    sheetId: "1iziPQCCuphRpCze78cyNpk--V5h915-ny89OcFdYCVo",
+    gid: "1604273665",
+    id: "A",
+    visitor: "B",
+    interviewee: "C",
+    beginTime: "D",
+    endTime: "E",
+    stayTime: "F",
+    comeCount: "G",
+    totalTime: "H",
+}
+
+
+
+
